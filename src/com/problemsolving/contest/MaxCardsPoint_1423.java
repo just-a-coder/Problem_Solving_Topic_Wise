8ac442849cf98aa2ec5d600c5cb4c50fae326b25
Just A Coder <54611515+just-a-coder@users.noreply.github.com>
2020-04-26 20:00:38 +0530

    maximum points can be obtained from cards

---
+++ src/com/problemsolving/contest/MaxCardsPoint_1423.java
@@ -0,0 +1,25 @@
+package com.problemsolving.contest;
+
+public class MaxCardsPoint_1423 {
+    public static int maxScore(int[] cardPoints, int k) {
+        int maxscore = 0, n = cardPoints.length;
+        int suffixsum = 0, prefixsum = 0;
+        for (int i = n - 1; i > n - 1 - k; i--) {
+            suffixsum += cardPoints[i];
+        }
+        maxscore = suffixsum;
+        for (int i = 0; i < k; i++) {
+            prefixsum += cardPoints[i];
+            suffixsum -= cardPoints[n - k + i];
+            maxscore = Math.max(maxscore, suffixsum + prefixsum);
+        }
+
+        return maxscore;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = new int[]{1, 2, 3, 4, 5, 6, 1};
+        int k = 3;
+        System.out.println(maxScore(arr, k));
+    }
+}
