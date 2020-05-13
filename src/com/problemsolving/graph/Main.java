d3d6d979e9af9b6bd3ae931863adb41df916260c
Just A Coder <54611515+just-a-coder@users.noreply.github.com>
2020-05-13 23:47:49 +0530

    graph

---
+++ src/com/problemsolving/graph/Main.java
@@ -0,0 +1,15 @@
+package com.problemsolving.graph;
+
+public class Main {
+    public static void main( String args[] ) {
+        Graph g= new Graph(4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(2, 3);
+        g.addEdge(3, 0);
+
+        g.printGraph();
+    }
+}
+
